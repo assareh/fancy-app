50fdb5c12ac7fc274f5f635520bc169b79276386
assareh <assareh@me.com>
2022-01-14 15:22:16 -0800

    1.0.1

--- service.tf
+++ service.tf
@@ -1,6 +1,6 @@
 module "my-fancy-service" {
   source  = "app.terraform.io/hashidemos/ecs-service/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   name        = var.name
   environment = var.environment
