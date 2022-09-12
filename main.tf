fd59562d79fbca6469f63f849e3139f843fa56de
assareh <assareh@hashicorp.com>
2022-09-12 16:44:24 -0700

    adding cloud block with tags

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ provider "aws" {
 resource "aws_vpc" "hashicat" {
   cidr_block = "10.0.0.0/16"
 }
+
+terraform {
+  cloud {
+    organization = "hashidemos"
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+
+    workspaces {
+      tags = ["fancy-app"]
+    }
+  }
+}
