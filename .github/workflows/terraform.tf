e94121b2c17386ea6bda7d61cd2bbc62fd194e49
assareh <assareh@hashicorp.com>
2022-09-15 20:43:55 -0700

    required versions

---
+++ .github/workflows/terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.31.0"
+    }
+    tfe = {
+      source = "hashicorp/tfe"
+      version = "~> 0.36.1"
+    }
+  }
+  required_version = ">= 1.0"
+}
